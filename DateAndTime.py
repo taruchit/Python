7aace1e5ffed1136a3a163fbac65567cda41b7e2
Taruchit Goyal <taruchit.goyal@gmail.com>
2021-04-18 22:08:14 +0530

    Update DateAndTime.py
    Formatting date and time in Python.

--- DateAndTime.py
+++ DateAndTime.py
@@ -31,3 +31,37 @@ print("Today's date and time': ",today)
 t=datetime.time(datetime.now())
 print("Current time: ",t)
 
+#Printing the current year
+now=datetime.now()
+print(now.strftime("The current (full) year is: %Y"))
+print(now.strftime("The current's last two digits: %y"))
+
+#Printing the current day
+print(now.strftime("The current day is: %A"))
+print(now.strftime("The current day in shortform is: %a"))
+
+#Printing the current month
+print(now.strftime("The current month is: %B"))
+print(now.strftime("The current month in short form is: %b"))
+
+#Pringting today's complete datedate
+print(now.strftime("Today's date mm/dd/yy: %D"))
+
+#Printing only date
+print(now.strftime("Date: %d"))
+
+#Printing current time
+print(now.strftime("Current time: %X"))
+
+#Printing today's date
+print(now.strftime("Printing today's date: %x"))
+
+#Printing day month date and time in short form
+print(now.strftime("Today's day, month, date and time in short form: %c"))
+
+#Printing whether current time is AM or PM
+print(now.strftime("AM or PM: %p"))
+
+#Printing current time
+print(now.strftime("Current time as per 12 hour window: %I:%M:%S %p"))
+print(now.strftime("Current time as per 24 hour window: %H:%M"))
