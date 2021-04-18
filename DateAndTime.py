8f8b904518619a12590ecab6bd94433d80668e9e
Taruchit Goyal <taruchit.goyal@gmail.com>
2021-04-18 22:35:32 +0530

    Update DateAndTime.py
    Using timedelata and relative date calculations

--- DateAndTime.py
+++ DateAndTime.py
@@ -8,6 +8,7 @@ Created on Sun Apr 18 00:47:14 2021
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
 
 # Printing today's date
 today=date.today()
@@ -65,3 +66,29 @@ print(now.strftime("AM or PM: %p"))
 #Printing current time
 print(now.strftime("Current time as per 12 hour window: %I:%M:%S %p"))
 print(now.strftime("Current time as per 24 hour window: %H:%M"))
+
+
+#Printing a basic timedelta
+print("Basic timedelta: ",timedelta(days=365, hours=5, minutes=1))
+
+now=datetime.now()
+#Printing the date after an year
+print("One year later: "+str(now+timedelta(days=365)))
+
+#Using timedelata with two arguments
+print("Date after 4 weeks and 4 days: "+str(now+timedelta(days=4, weeks=4)))
+
+#Printing the date 1 week ago
+print("One week ago, the date was: "+str(now-timedelta(weeks=1)))
+
+#Printing the number of days between current date and birthday date
+today=date.today()
+birthdayDate = date(today.year,9,1)
+print("Birthday date: ",birthdayDate)
+if today<birthdayDate:
+    print("Number of days for the birthday: ",(birthdayDate-today).days)
+elif today==birthdayDate:
+    print("Today is the birthday.")
+else:
+    print("Birthday for this year happend %d days ago" %((today-birthdayDate).days))
+    
